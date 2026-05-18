472ea36988e8148a6a13c3302336b8560d65ebe0
Anushree <anushreemishra619@gmail.com>
2026-05-18 20:16:45 +0530

    updated strcuture

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,13 +7,11 @@ from app.routes.crit_score_route import router as score_router
 from app.routes.api_route import router as api_router
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
